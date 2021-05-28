b147ca56b77842ab3a0354c0fe5d6be77e24b192
eiji <eijishimizu9@gmail.com>
2021-05-28 16:50:44 +0900

    c++17の入れ子名前空間を導入

--- include/Configuration/Configuration.h
+++ include/Configuration/Configuration.h
@@ -5,50 +5,46 @@
 
 #include <vector>
 
-namespace Eihire
+namespace Eihire::Configuration
 {
-    namespace Configuration
+    class Configuration
     {
+    public:
+        Configuration();
+        ~Configuration();
+        Configuration(const std::string &fileName);
+        Configuration(const std::vector<std::string> &fileNameList);
 
-        class Configuration
-        {
-        public:
-            Configuration();
-            ~Configuration();
-            Configuration(const std::string &fileName);
-            Configuration(const std::vector<std::string> &fileNameList);
+        // コピー禁止
+        Configuration(const Configuration &) = delete;
+        Configuration &operator=(const Configuration &) = delete;
+        // ムーブ禁止
+        Configuration(Configuration &&) = delete;
+        Configuration &operator=(Configuration &&) = delete;
 
-            // コピー禁止
-            Configuration(const Configuration &) = delete;
-            Configuration &operator=(const Configuration &) = delete;
-            // ムーブ禁止
-            Configuration(Configuration &&) = delete;
-            Configuration &operator=(Configuration &&) = delete;
+        // 引数のkeyに対応する設定値を探しその値を返す
+        // 無い場合は空文字列を返す
+        std::string find(const std::string &key) const;
+        // 第1引数でプロパティファイル名を指定して,第2引数のkeyに対応する設定値を返す
+        // 無い場合は空文字列を返す
+        std::string find(const std::string &fileName, const std::string &key) const;
 
-            // 引数のkeyに対応する設定値を探しその値を返す
-            // 無い場合は空文字列を返す
-            std::string find(const std::string &key) const;
-            // 第1引数でプロパティファイル名を指定して,第2引数のkeyに対応する設定値を返す
-            // 無い場合は空文字列を返す
-            std::string find(const std::string &fileName, const std::string &key) const;
+        // 引数のkeyに対応する設定値を探しその値を返す
+        // 無い場合は例外を投げる
+        std::string get(const std::string &key) const;
+        // 第1引数でプロパティファイル名を指定して,第2引数のkeyに対応する設定値を返す
+        // 無い場合は例外を投げる
+        std::string get(const std::string &fileName, const std::string &key) const;
 
-            // 引数のkeyに対応する設定値を探しその値を返す
-            // 無い場合は例外を投げる
-            std::string get(const std::string &key) const;
-            // 第1引数でプロパティファイル名を指定して,第2引数のkeyに対応する設定値を返す
-            // 無い場合は例外を投げる
-            std::string get(const std::string &fileName, const std::string &key) const;
+        const std::vector<PropertiesMap> &propertiesMapList() const;
 
-            const std::vector<PropertiesMap> &propertiesMapList() const;
+    private:
+        void initialize();
 
-        private:
-            void initialize();
+        std::vector<PropertiesMap> propertiesMapList_;
+        std::vector<std::string> fileNameList_;
+    };
 
-            std::vector<PropertiesMap> propertiesMapList_;
-            std::vector<std::string> fileNameList_;
-        };
-
-    } // namespace Configuration
-} // namespace Eihire
+} // namespace Eihire::Configuration
 
 #endif // EIHIRE_CONFIGURATION_Configuration_INCLUDED
--- src/Configuration/Configuration.cpp
+++ src/Configuration/Configuration.cpp
@@ -3,119 +3,116 @@
 
 #include <iostream>
 
-namespace Eihire
+namespace Eihire::Configuration
 {
-    namespace Configuration
+    namespace
     {
-        namespace
+        void initializeImpl(std::vector<PropertiesMap> &propertiesMapList, const std::vector<std::string> &fileNameList)
         {
-            void initializeImpl(std::vector<PropertiesMap> &propertiesMapList, const std::vector<std::string> &fileNameList)
+            for (const auto &fileName : fileNameList)
             {
-                for (const auto &fileName : fileNameList)
-                {
-                    PropertiesMap p{fileName};
-                    propertiesMapList.push_back(p);
-                }
-                for (PropertiesMap &p : propertiesMapList)
-                {
-                    p.load();
-                }
+                PropertiesMap p{fileName};
+                propertiesMapList.push_back(p);
             }
-
-        } // namespace
-
-        Configuration::Configuration()
-            : fileNameList_{"Eihire.properties"}
-        {
-            initialize();
-        }
-
-        Configuration::~Configuration()
-        {
-            // noop
-        }
-
-        Configuration::Configuration(const std::string &fileName)
-            : fileNameList_{fileName}
-        {
-            initialize();
-        }
-
-        Configuration::Configuration(const std::vector<std::string> &fileNameList)
-            : fileNameList_{fileNameList}
-        {
-            initialize();
-        }
-
-        std::string Configuration::find(const std::string &key) const
-        {
-            return find("", key);
-        }
-
-        std::string Configuration::find(const std::string &fileName, const std::string &key) const
-        {
-            for (const PropertiesMap &p : propertiesMapList_)
+            for (PropertiesMap &p : propertiesMapList)
             {
-                if (fileName != "")
-                {
-                    if (p.fileName() == fileName)
-                    {
-                        if (p.isContain(key))
-                            return p.get(key);
-                        return "";
-                    }
-                    else
-                        continue;
-                }
-
-                if (p.isContain(key))
-                    return p.get(key);
-                else
-                    continue;
+                p.load();
             }
-            return "";
         }
 
-        std::string Configuration::get(const std::string &key) const
-        {
-            return get("", key);
-        }
+    } // namespace
 
-        std::string Configuration::get(const std::string &fileName, const std::string &key) const
+    Configuration::Configuration()
+        : fileNameList_{"Eihire.properties"}
+    {
+        initialize();
+    }
+
+    Configuration::~Configuration()
+    {
+        // noop
+    }
+
+    Configuration::Configuration(const std::string &fileName)
+        : fileNameList_{fileName}
+    {
+        initialize();
+    }
+
+    Configuration::Configuration(const std::vector<std::string> &fileNameList)
+        : fileNameList_{fileNameList}
+    {
+        initialize();
+    }
+
+    std::string Configuration::find(const std::string &key) const
+    {
+        return find("", key);
+    }
+
+    std::string Configuration::find(const std::string &fileName, const std::string &key) const
+    {
+        for (const PropertiesMap &p : propertiesMapList_)
         {
             if (fileName != "")
             {
-                for (const PropertiesMap &p : propertiesMapList_)
+                if (p.fileName() == fileName)
                 {
-                    if (p.fileName() == fileName)
+                    if (p.isContain(key))
                         return p.get(key);
-                    else
-                        continue;
+                    return "";
                 }
-                // 見つからなかった場合に強制的にout_of_range例外を送出
-                return propertiesMapList_.at(propertiesMapList_.size()).properties().at(key);
+                else
+                    continue;
             }
-            
+
+            if (p.isContain(key))
+                return p.get(key);
+            else
+                continue;
+        }
+        return "";
+    }
+
+    std::string Configuration::get(const std::string &key) const
+    {
+        return get("", key);
+    }
+
+    std::string Configuration::get(const std::string &fileName, const std::string &key) const
+    {
+        if (fileName != "")
+        {
             for (const PropertiesMap &p : propertiesMapList_)
             {
-                if (p.isContain(key))
+                if (p.fileName() == fileName)
                     return p.get(key);
                 else
                     continue;
             }
             // 見つからなかった場合に強制的にout_of_range例外を送出
-            return propertiesMapList_.at(0).properties().at(key);
+            return propertiesMapList_.at(propertiesMapList_.size()).properties().at(key);
         }
 
-        const std::vector<PropertiesMap> &Configuration::propertiesMapList() const
+        for (const PropertiesMap &p : propertiesMapList_)
         {
-            return propertiesMapList_;
+            if (p.isContain(key))
+                return p.get(key);
+            else
+                continue;
         }
+        // 見つからなかった場合に強制的にout_of_range例外を送出
+        return propertiesMapList_.at(0).properties().at(key);
+    }
 
-        void Configuration::initialize()
-        {
-            initializeImpl(propertiesMapList_, fileNameList_);
-        }
+    const std::vector<PropertiesMap> &Configuration::propertiesMapList() const
+    {
+        return propertiesMapList_;
+    }
 
-    } // Configuration
-} // Eihire
+    void Configuration::initialize()
+    {
+        initializeImpl(propertiesMapList_, fileNameList_);
+    }
+
+} // namespace Eihire::Configuration
