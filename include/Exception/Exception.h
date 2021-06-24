194cc27e305217361b6bca918210b55b0321c21f
eiji <eijishimizu9@gmail.com>
2021-06-24 12:31:24 +0900

    例外基底クラスの実装

--- include/Exception/Exception.h
+++ include/Exception/Exception.h
@@ -4,27 +4,49 @@
 #include <stdexcept>
 #include <string>
 #include <filesystem>
+#include <type_traits>
 
 #define E_EXCEPTION_BASE_ARGS(MESSAGE, INNER_EXCEPTION) (strrchr(__FILE__, std::filesystem::path::preferred_separator) ? strrchr(__FILE__, std::filesystem::path::preferred_separator) + 1 : __FILE__), __FUNCTION__, __LINE__, MESSAGE, INNER_EXCEPTION
 
 namespace Eihire::Exception
 {
-    template <typename InnerException>
-    class ExceptionBase : public std::runtime_error
+    class ExceptionBase : public std::exception
     {
     public:
+        template <typename InnerException, typename = std::enable_if_t<std::is_base_of_v<std::exception, InnerException>>>
         ExceptionBase(const char *fileName,
                       const char *functionName,
                       uint64_t line,
                       const std::string &message,
                       const InnerException &inner)
-            : std::runtime_error{std::string{fileName} + " " + functionName + "(" + std::to_string(line) + ") " + message},
-              inner_(inner)
+            : messages_{getFormatMessage(fileName, functionName, line, message) + '\n' + inner.what()}
         {
+            // static_assert(std::is_base_of_v<std::exception, InnerException> == true, "Parameter type InnerException is required derived from std::exception");
+        }
+
+        const char *what() const noexcept override
+        {
+            return messages_.c_str();
+        }
+
+        // コピー演算
+        ExceptionBase(const ExceptionBase &) = default;
+        ExceptionBase &operator=(const ExceptionBase &) = default;
+        // ムーブ演算
+        ExceptionBase(ExceptionBase &&) = default;
+        ExceptionBase &operator=(ExceptionBase &&) = default;
+
+    protected:
+        virtual std::string getFormatMessage(const char *fileName,
+                                             const char *functionName,
+                                             uint64_t line,
+                                             const std::string &message) const
+        {
+            return std::string{fileName} + " " + functionName + "(" + std::to_string(line) + ") " + message;
         }
 
     private:
-        const InnerException inner_;
+        std::string messages_;
     };
 
     class FileCannotOpenException : public std::runtime_error
