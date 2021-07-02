46f00186006a3dec415da66c6018c781191dd261
eiji <eijishimizu9@gmail.com>
2021-07-02 16:16:35 +0900

    入れ子の例外クラスの処理はstd::throw_with_nestedを利用するように修正

--- include/Exception/Exception.h
+++ include/Exception/Exception.h
@@ -1,44 +1,68 @@
 #ifndef EIHIRE_EXCEPTION_Exception_INCLUDED
 #define EIHIRE_EXCEPTION_Exception_INCLUDED
 
+#include <filesystem>
+#include <sstream>
 #include <stdexcept>
 #include <string>
-#include <filesystem>
 #include <type_traits>
 
 #define E_EXCEPTION_BASE_ARGS(MESSAGE) (strrchr(__FILE__, std::filesystem::path::preferred_separator) ? strrchr(__FILE__, std::filesystem::path::preferred_separator) + 1 : __FILE__), __FUNCTION__, __LINE__, MESSAGE
 
-namespace Eihire::Exception
-{
-    class ExceptionBase : public std::exception
-    {
-    public:
-        template <typename InnerException, typename = std::enable_if_t<std::is_base_of_v<std::exception, InnerException>>>
-        ExceptionBase(const char *fileName,
-                      const char *functionName,
-                      uint64_t line,
-                      const std::string &message,
-                      const InnerException &inner)
-            : messages_{getFormatMessage(fileName, functionName, line, message) + '\n' + inner.what()}
-        {
-        }
+namespace Eihire::Exception {
 
+    template <typename T>
+    std::enable_if_t<std::is_base_of_v<std::exception, T>, const std::string>
+    getAllMessages(const T &e)
+    {
+        std::ostringstream oss{};
+        oss << e.what();
+        if (dynamic_cast<const std::nested_exception *>(&e) != nullptr) {
+            oss << std::endl
+                << getAllMessages((dynamic_cast<const std::nested_exception &>(e).nested_ptr()));
+        }
+        return oss.str();
+    }
+
+    inline const std::string getAllMessages(const std::exception_ptr &rp)
+    {
+        if (!rp)
+            return "";
+        std::ostringstream oss{};
+        try {
+            std::rethrow_exception(rp);
+        }
+        catch (const std::exception &e) {
+            oss << e.what();
+            if (dynamic_cast<const std::nested_exception *>(&e) != nullptr) {
+                oss << std::endl
+                    << getAllMessages((dynamic_cast<const std::nested_exception &>(e).nested_ptr()));
+            }
+        }
+        catch (...) {
+            return "";
+        }
+        return oss.str();
+    }
+
+    class ExceptionBase : public std::runtime_error {
+    public:
         ExceptionBase(const char *fileName,
                       const char *functionName,
                       uint64_t line,
                       const std::string &message)
-            : messages_{getFormatMessage(fileName, functionName, line, message)}
+            : std::runtime_error{getFormatMessage(fileName, functionName, line, message)}
         {
         }
 
         ExceptionBase(const std::string &message)
-            : messages_{message}
+            : std::runtime_error{message}
         {
         }
 
         const char *what() const noexcept override
         {
-            return messages_.c_str();
+            return std::runtime_error::what();
         }
 
     private:
@@ -49,23 +73,10 @@ namespace Eihire::Exception
         {
             return std::string{fileName} + " " + functionName + "(" + std::to_string(line) + ") " + message;
         }
-
-        std::string messages_;
     };
 
-    class FileCannotOpenException : public ExceptionBase
-    {
+    class FileCannotOpenException : public ExceptionBase {
     public:
-        template <typename InnerException, typename = std::enable_if_t<std::is_base_of_v<std::exception, InnerException>>>
-        FileCannotOpenException(const char *fileName,
-                                const char *functionName,
-                                uint64_t line,
-                                const std::string &message,
-                                const InnerException &inner)
-            : ExceptionBase{fileName, functionName, line, prefix() + message, inner}
-        {
-        }
-
         FileCannotOpenException(const char *fileName,
                                 const char *functionName,
                                 uint64_t line,
@@ -86,19 +97,8 @@ namespace Eihire::Exception
         }
     };
 
-    class ParseException : public ExceptionBase
-    {
+    class ParseException : public ExceptionBase {
     public:
-        template <typename InnerException, typename = std::enable_if_t<std::is_base_of_v<std::exception, InnerException>>>
-        ParseException(const char *fileName,
-                       const char *functionName,
-                       uint64_t line,
-                       const std::string &message,
-                       const InnerException &inner)
-            : ExceptionBase{fileName, functionName, line, prefix() + message, inner}
-        {
-        }
-
         ParseException(const char *fileName,
                        const char *functionName,
                        uint64_t line,
